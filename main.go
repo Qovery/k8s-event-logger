c4057073e304c797691eca66de69a52a4c1b2254
Pierre Gerbelot <pgerbelot@qovery.com>
2025-06-27 16:56:57 +0200

    feat(obs): WIP

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"flag"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -16,6 +21,31 @@ import (
 var (
 	ignoreNormal = flag.Bool("ignore-normal", false, "ignore events of type 'Normal' to reduce noise")
 	ignoreUpdate = flag.Bool("ignore-update", true, "ignore update of events")
+	metricsPort  = flag.String("metrics-port", "8080", "port for prometheus metrics endpoint")
+)
+
+var (
+	eventsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "q_k8s_events_total",
+			Help: "Total number of Kubernetes events processed",
+		},
+		[]string{"type", "reason", "namespace", "object_kind", "object_name"},
+	)
+
+	eventsProcessed = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "q_k8s_events_processed_total",
+			Help: "Total number of events processed by the watcher",
+		},
+	)
+
+	eventsIgnored = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "q_k8s_events_ignored_total",
+			Help: "Total number of events ignored due to filters",
+		},
+	)
 )
 
 func main() {
@@ -23,6 +53,14 @@ func main() {
 
 	loggerApplication := log.New(os.Stderr, "", log.LstdFlags)
 	loggerEvent := log.New(os.Stdout, "", 0)
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		loggerApplication.Printf("Starting metrics server on port %s", *metricsPort)
+		if err := http.ListenAndServe(":"+*metricsPort, nil); err != nil {
+			loggerApplication.Printf("Failed to start metrics server: %v", err)
+		}
+	}()
 
 	// Using First sample from https://pkg.go.dev/k8s.io/client-go/tools/clientcmd to automatically deal with environment variables and default file paths
 
@@ -63,6 +101,7 @@ func main() {
 					return
 				}
 				logEvent(obj, loggerEvent)
+				recordEventMetrics(obj.(*corev1.Event))
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
 				if *ignoreUpdate || (*ignoreNormal && newObj.(*corev1.Event).Type == corev1.EventTypeNormal) {
@@ -82,4 +121,26 @@ func main() {
 func logEvent(obj interface{}, logger *log.Logger) {
 	j, _ := json.Marshal(obj)
 	logger.Printf("%s\n", string(j))
+}
+
+func recordEventMetrics(event *corev1.Event) {
+	// Incrémenter le compteur total d'événements traités
+	eventsProcessed.Inc()
+
+	// Extraire les informations de l'objet concerné
+	objectKind := event.InvolvedObject.Kind
+	objectName := event.InvolvedObject.Name
+	namespace := event.Namespace
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	// Incrémenter le compteur avec les labels spécifiques
+	eventsTotal.WithLabelValues(
+		event.Type,
+		event.Reason,
+		namespace,
+		objectKind,
+		objectName,
+	).Inc()
 }
